1ba531fbbb473d41baa046b387512111d747af41
Bartossh <lenart.consulting@gmail.com>
2023-12-28 14:15:18 +0100

    5: Update makefile with vendors packages.

--- src/main.c
+++ src/main.c
@@ -7,24 +7,25 @@
 int main(int argc, char* argv[]) 
 { 
     if( argc < 2 ) {
-        printf("Please provide password to be validated.\n");
+        printf("ERROR: Please provide password to be validated.\n");
         return 1;
     }
-    char tbo[PasswordLength];
     switch (SE_check()){
     case 0:
-        printf("All modules are ready.\n");
+        printf("INFO: All modules are ready.\n");
         break;
     case SE_ErrPownedModuleNotReady:
-        printf("Module Pawned Passwords is not ready yet.\n");
+        printf("INFO: Module Pawned Passwords is not ready yet.\n");
         break;
     default:
-        printf("Unknown check status. Some modules may not work properly.\n");
+        printf("FATAL: Unknown check status. Some modules may not work properly.\n");
+        return 1;
     }
   
+    char tbo[PasswordLength];
     snprintf(tbo, PasswordLength*sizeof(char), "%s", argv[1]); 
 
-    printf("Given password [ %s ] is going to be validated...\n", tbo);  
+    printf("INFO: Validating password: [ %s ] ...\n", tbo);  
   
     return 0; 
 }
