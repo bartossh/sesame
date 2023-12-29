7235cc55483d6a2ac54319ca58d43c89a4ff5ef0
Bartossh <lenart.consulting@gmail.com>
2023-12-29 13:45:32 +0100

    7: Implemented Shannon entropy calculation for password passed as an argument.

--- src/sesame.h
+++ src/sesame.h
@@ -1,11 +1,15 @@
 #ifndef SESAME_H
 #define SESAME_H
 
-#define SE_ErrPownedModuleNotReady 1
-
 /// SESAME_check checks which modules are working.
 ///
 /// Returns 0 if OK, or Error otherwise.
 int SE_check(void);
+
+/// SE_shannon calculates Shannon information entropy of given character string.
+///
+/// str - nullable string of characters.
+unsigned long 
+SE_shannon(unsigned char *str);
 
 #endif
--- src/sesame_test.c
+++ src/sesame_test.c
@@ -1,27 +1,85 @@
+#include <sys/time.h>
+#include <strings.h> 
 #include "../test-framework/unity.h"
 #include "../vendor/log.c/src/log.h"
 #include "sesame.h"
 
-void setUp(void)
-{
+void setUp(void) {
 }
 
-void tearDown(void)
-{
+void tearDown(void) {
 }
 
-static void test_basic(void)
-{
+static void testBasic(void) {
     log_info("Testing SE_check() function.\n");
     int result = SE_check();
     TEST_ASSERT_EQUAL(SE_ErrPownedModuleNotReady, result);
+}
+
+static void testShannon(void) {
+    typedef struct testPair {
+        const char *password;
+        size_t entropy;
+    } TestPair;
+
+    TestPair testCases[5] = {
+        {
+            .password = "password",
+            .entropy = 24
+        },
+        {
+            .password = "123",
+            .entropy = 6
+        },
+        {
+            .password = "myCa7I5a60d",
+            .entropy = 44
+        },
+        {
+            .password = "IthinkItIsVeryStrong",
+            .entropy = 80
+        },
+        {
+            .password = "7k289be923hv934",
+            .entropy = 60
+        }
+    };
+
+    log_info("Testing SE_shannon() function.\n");
+    for (size_t tc = 0; tc < 5; tc++) {
+        unsigned long result = SE_shannon((unsigned char*)testCases[tc].password);
+        printf("Password [ %s ] = [ %lu ] bits of entropy.\n", testCases[tc].password, result);
+        TEST_ASSERT_EQUAL(testCases[tc].entropy, result);
+    }
+}
+
+static void benchShannon(void) {
+    const size_t iter = 1000;
+    struct timeval begin, end;
+    gettimeofday(&begin, 0);
+    log_info("Benchmarking SE_shannon() function.\n");
+    char psswd[] = "someLongPasswordToBench";
+    for (size_t tc = 0; tc < iter; tc++) {
+        unsigned long result = SE_shannon((unsigned char*)psswd);
+        TEST_ASSERT_EQUAL(92, result);
+    }
+
+    gettimeofday(&end, 0);
+    long seconds = end.tv_sec - begin.tv_sec;
+    long microseconds = end.tv_usec - begin.tv_usec;
+    double elapsed = seconds + microseconds*1e-6;
+    log_info("Calculating Shannon bits entropy for %lu passwords took [ %f_sec ]\n", iter, elapsed);
+    log_info("Shannon entropy took [ %f_sec ] per password of length of %lu characters.\n", elapsed/(double)iter, strlen(psswd));
 }
 
 int main(void)
 {
     UnityBegin("sesame_test.c");
 
-    RUN_TEST(test_basic);
+    RUN_TEST(testBasic);
+    RUN_TEST(testShannon);
+    
+    RUN_TEST(benchShannon);
    
     return UnityEnd();
 }
